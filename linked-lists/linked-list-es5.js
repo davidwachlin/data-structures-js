1ad17697c8ca0974eaa91b24c0c8cc6f455d6b5d
David Wachlin <davidwachlin@gmail.com>
2019-09-29 15:10:15 -0400

    es5 linked list

--- linked-lists/linked-list-es5.js
+++ linked-lists/linked-list-es5.js
@@ -1,20 +1,20 @@
-// DOUBLY LINKED LIST
+//Linked Lists
 
-//constructor functions
+//function constructors
 function LinkedList() {
     this.head = null;
     this.tail = null;
-};
+}
 
-function LinkedListNode(value, next, prev) {
+function Node(value, next, prev) {
     this.value = value;
     this.next = next;
     this.prev = prev;
-};
-// Add To Head
+}
 
+// Add To Head
 LinkedList.prototype.addToHead = function(value) {
-    let newNode = new LinkedListNode(value, this.head, null);
+    var newNode = new Node(value, this.head, null);
     if (this.head) this.head.prev = newNode;
     else this.tail = newNode;
     this.head = newNode;
@@ -22,23 +22,50 @@ LinkedList.prototype.addToHead = function(value) {
 
 // Add To Tail
 LinkedList.prototype.addToTail = function(value) {
-    let newNode = newLinkedListNode(value, null, this.tail);
+    var newNode = new Node(value, null, this.tail);
     if (this.tail) this.tail.next = newNode;
-    else this.head = newNode;
-    this.tail = newNode
+    else this.head = newNode
+    this.tail = newNode;
 }
+
 // Remove Head
-LinkedList.prototype.removeHead - function (value) {
+LinkedList.prototype.removeHead = function() {
     if (!this.head) return null;
-    let val = this.head.value;
+    var val = this.head.value;
     this.head = this.head.next;
     if (this.head) this.head.prev = null;
     else this.tail = null;
     return val;
 }
 
-
 // Remove Tail
+LinkedList.prototype.removeTail = function() {
+    if (!this.tail) return null;
+    var val = this.tail.value;
+    this.tail = this.tail.prev;
+}
 
 // Search
+LinkedList.prototype.search = function(searchValue) {
+    var currentNode = this.head;
+    while (currentNode) {
+        if (currentNode.value === searchValue) return currentNode.value;
+        curentNode = currentNode.next
+    }
+    return null;
+}
+// indexOf
 
+LinkedList.prototype.indexOf = function(value) {
+    var indexes = [];
+    var currentIndex = 0;
+    var currentNode = this.head;
+    while (currentNode) {
+        if (currentNode.value === value) {
+            indexes.push(currentIndext)
+        }
+        currentNode = currentNode.next;
+        currentIndex++;
+    }
+    return indexes;
+}
--- linked-lists/linked-list-starter-es5.js
+++ linked-lists/linked-list-starter-es5.js
@@ -12,3 +12,5 @@
 
 // Search
 
+// indexOf
+
