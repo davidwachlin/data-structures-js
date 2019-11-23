e7e742b3f49cf5f4f793d9f6f0bc9b7195bfbb80
David Wachlin <davidwachlin@gmail.com>
2019-11-23 17:54:53 -0500

    stack

--- stack/stack.js
+++ stack/stack.js
@@ -1,14 +1,26 @@
 class Stack {
 	constructor() {
 		// create our stack, which is an empty object
-		this.stack = {};
+		this._storage = {};
+		this.length = 0;
 	}
 	// this method will push a value onto the top of our stack
-    push(value) {}
-    
+	push(value) {
+		this._storage[this._length] = value;
+		this.length++;
+	}
+
 	// this method is responsible for popping off the last value and returning it
-	pop() {}
+	pop() {
+		const lastVal = this._storage[--this._length];
+		delete this._storage[--this._length];
+		this._length--;
+		return lastVal;
+	}
 
 	// this will peek at the last value added to the stack
-	peek() {}
+	peek() {
+		const lastVal = this._storage[--this._length];
+		return lastVal;
+	}
 }
