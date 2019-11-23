90d60fcc68d38c3b6ff2a452e3a903b6553c4729
David Wachlin <davidwachlin@gmail.com>
2019-11-23 17:46:59 -0500

    stack template

---
+++ stack/stack.js
@@ -0,0 +1,14 @@
+class Stack {
+	constructor() {
+		// create our stack, which is an empty object
+		this.stack = {};
+	}
+	// this method will push a value onto the top of our stack
+    push(value) {}
+    
+	// this method is responsible for popping off the last value and returning it
+	pop() {}
+
+	// this will peek at the last value added to the stack
+	peek() {}
+}
